8505a5e0a57f4d4e9c16323ecd4d7fd3640e5829
rdstonech <rdstonech@gmail.com>
2018-03-11 12:56:06 +0300

    Что за разнообразие стилей?

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -1,12 +1,11 @@
-void setup() {
+void setup (){
   Serial.begin (9600);
 
   pinMode (LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  if (Serial.available ())
-  {
+void loop (){
+  if (Serial.available ()){
     Serial.println (Serial.read ());
 
     digitalWrite (LED_BUILTIN, HIGH);
